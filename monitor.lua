41dc2152eb4b5ad76de5ff8cb019c2755847606a
Akminime <125833644+Akminime@users.noreply.github.com>
2024-02-25 18:34:57 -0900

    test4

--- monitor.lua
+++ monitor.lua
@@ -1,10 +1,8 @@
-local monitor = peripheral.find("monitor_1")
-monitor.setCursorPos(1, 1)
-monitor.write("Locating Chest...")
-local chest = peripheral.find("minecraft:chest")
+local monitor = peripheral.find("monitor_2")
 
-if chest == nil then
-    monitor.write("No Chest Found!")
+if monitor then
+    monitor.setCursorPos(1, 1)
+    monitor.write("Hello, world!")
 else
-    monitor.write(chest.size())
+    print("Monitor not found.")
 end
